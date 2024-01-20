7ccca85dfa7cd7cc08e38403f1fc5ea85bfdd353
Katharine Cordes <gillis.katharine@gmail.com>
2024-01-20 10:55:13 -0700

    feat: add commandline parser localization

--- Console/Controllers/FeatureFlags/Get/Verb.cs
+++ Console/Controllers/FeatureFlags/Get/Verb.cs
@@ -1,12 +1,16 @@
 using CommandLine;
 using Console.Common;
+using Console.Localization;
 
 namespace Console.Controllers.FeatureFlags.Get;
 
-[Verb("featureflag:get", HelpText = "Get a feature flag")]
+[Verb("featureflag:get", HelpText = "HelpGetFeatureFlagVerb",
+    ResourceType = typeof(Resources.CommandLineParser))]
 public sealed class Verb : IHasControllerType, IOptions
 {
     public Type ControllerType { get; } = typeof(Controller);
 
-    [Option('i', "id", Required = true)] public string Id { get; set; } = null!;
+    [Option('i', "id", Required = true, HelpText = "HelpIdOption",
+        ResourceType = typeof(Resources.CommandLineParser))]
+    public string Id { get; set; } = null!;
 }
--- Console/Controllers/FeatureFlags/Update/Verb.cs
+++ Console/Controllers/FeatureFlags/Update/Verb.cs
@@ -3,13 +3,17 @@ using Console.Common;
 
 namespace Console.Controllers.FeatureFlags.Update;
 
-[Verb("featureflag:update", HelpText = "Update a feature flag")]
+[Verb("featureflag:update", HelpText = "HelpUpdateFeatureFlagVerb",
+    ResourceType = typeof(Resources.CommandLineParser))]
 public class Verb : IHasControllerType, IOptions
 {
     public Type ControllerType { get; } = typeof(Controller);
 
-    [Option('i', "id", Required = true)] public string Id { get; set; } = null!;
+    [Option('i', "id", Required = true, HelpText = "HelpIdOption",
+        ResourceType = typeof(Resources.CommandLineParser))]
+    public string Id { get; set; } = null!;
 
-    [Option('e', "enabled", Default = false)]
+    [Option('e', "enabled", Default = false, HelpText = "HelpEnableOption",
+        ResourceType = typeof(Resources.CommandLineParser))]
     public bool? Enabled { get; set; }
 }
